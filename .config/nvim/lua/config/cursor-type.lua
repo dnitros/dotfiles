57813245183a0a07f726932c6666b8f1b6741652
Digvijay Singh <27089128+dnitros@users.noreply.github.com>
2025-10-05 19:33:19 +0530

    update tmux and neovim config to always have beam cursor type

--- .config/nvim/lua/config/cursor-type.lua
+++ .config/nvim/lua/config/cursor-type.lua
@@ -1,7 +1,7 @@
 -- Vertical cursor everywhere, but blinking only in insert mode
-vim.opt.guicursor = "n-v-c-sm:ver25-blinkon0,i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250,r-cr-o:ver25-blinkon0"
+vim.opt.guicursor = "n-v-c-sm:ver25-blinkon0,i-ci-ve:ver25-blinkon0,r-cr-o:ver25-blinkon0,a:ver25-blinkon0"
 
-vim.api.nvim_create_autocmd("VimLeave", {
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend", "VimResume" }, {
   pattern = "*",
-  command = "set guicursor=a:ver25",
+  command = "set guicursor=a:ver25-blinkon0",
 })
